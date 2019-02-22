b0b317c6d8c916fff75b5d9f93625c2f2cbbfc5a
Christian Gioia <christiang101@gmail.com>
2019-02-22 11:51:46 +1100

    Passport style authentication using cookies/sessions

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const session = require('express-sesion');
+const session = require('express-session');
 const passportLocalMongoose = require('passport-local-mongoose');
+const passport = require("passport");
 
 
 const app = express();
@@ -14,10 +15,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
 
 app.use(session ({
-  secret: process.env.SECRET,
+  secret: "Our little secret",
   resave: false,
   saveUninitialized: false
 }));
@@ -42,6 +44,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
 
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema( {
   email: String,
   password: String
@@ -51,12 +55,58 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/secrets", function(req,res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req,res) {
+  req.logout();
+  res.redirect("/");
+});
 
 app.post("/register", function(req, res) {
 
+  User.register(  {username: req.body.username}, req.body.password, function(err, user){
+
+    if (err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
 
 app.post("/login" , function(req,res){
+
+  const user = new User ({
+  username: req.body.username,
+  password: req.body.password
+});
+
+  req.login(user, function(err){
+    if (err){
+      console.log(err);
+    }
+    else {
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
 
 
